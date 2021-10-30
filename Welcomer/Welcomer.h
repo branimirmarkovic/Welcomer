99fc47412dac191eac044311e3e6e1a5de84229c
Branimir Makovic <branimir.markovic@ezderm.com>
2021-10-30 14:40:21 +0200

    Initial Commit

---
+++ Welcomer/Welcomer.h
@@ -0,0 +1,18 @@
+//
+//  Welcomer.h
+//  Welcomer
+//
+//  Created by Branimir Markovic on 30.10.21..
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Welcomer.
+FOUNDATION_EXPORT double WelcomerVersionNumber;
+
+//! Project version string for Welcomer.
+FOUNDATION_EXPORT const unsigned char WelcomerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Welcomer/PublicHeader.h>
+
+
